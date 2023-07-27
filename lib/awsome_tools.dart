20b5d50ab81def2cd43383fa49ce7f7a1750e2d2
moh <moh94syed@gmail.com>
2023-07-27 15:35:24 +0200

    add

--- lib/awsome_tools.dart
+++ lib/awsome_tools.dart
@@ -1,5 +1,6 @@
 export './src/config/config_awsome.dart';
 export './src/constants/constants.dart';
+export './src/widgets/txt_awsome.dart';
 export './src/widgets/custom_circle_awsome.dart';
 export './src/widgets/error_awsome.dart';
 export './src/widgets/loading_awsome.dart';
