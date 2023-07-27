e570a01fbe5eba58b985d8064faf6dbcb9122b8f
moh <moh94syed@gmail.com>
2023-07-27 15:31:56 +0200

    add

--- lib/src/style/app_colors.dart
+++ lib/src/style/app_colors.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors({
-    this.primaryColor = Colors.blue,
-    this.secondaryColor = Colors.blue,
+    this.primaryColor = Colors.pink,
+    this.secondaryColor = Colors.pinkAccent,
+
+    ///
     this.lightScaffoldBackgroundColor =
         const Color.fromARGB(255, 252, 250, 250),
     this.darkScaffoldBackgroundColor = const Color.fromARGB(255, 185, 185, 185),
   });
+
+  ///
   final Color primaryColor;
   final Color secondaryColor;
 
