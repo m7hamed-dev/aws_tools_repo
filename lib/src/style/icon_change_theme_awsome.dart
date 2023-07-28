c397a73ad878e39fcdd48043843e5e0e52f81c6e
moh <moh94syed@gmail.com>
2023-07-28 05:52:44 +0200

    add

---
+++ lib/src/style/icon_change_theme_awsome.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'theme_provider.dart';
+
+class IconChangeThemeAwsome extends StatefulWidget {
+  const IconChangeThemeAwsome({super.key});
+
+  @override
+  State<IconChangeThemeAwsome> createState() => _IconChangeThemeAwsomeState();
+}
+
+class _IconChangeThemeAwsomeState extends State<IconChangeThemeAwsome> {
+  @override
+  Widget build(BuildContext context) {
+    // Access the theme data from the InheritedWidget
+    final theme = ThemeProviderAwsome.of(context).theme;
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.brightness_4), // Toggle theme icon (moon icon)
+    );
+  }
+
+  // Toggle the theme mode when the FAB is pressed
+  // void _toggleTheme(ThemeProviderAwsome theme) {
+  //   if (theme.theme) {}
+  //   setState(() {});
+  // }
+}
