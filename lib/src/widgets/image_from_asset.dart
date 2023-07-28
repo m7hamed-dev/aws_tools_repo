76c72a629bed68a4894d19ad3368b92e7875c309
moh <moh94syed@gmail.com>
2023-07-28 08:59:49 +0200

    add

---
+++ lib/src/widgets/image_from_asset.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ImageFromAsset extends StatelessWidget {
+  const ImageFromAsset({
+    super.key,
+    required this.path,
+    this.color,
+    this.width,
+    this.height,
+    this.fit,
+  });
+  final Color? color;
+  final double? width, height;
+  final String path;
+  final BoxFit? fit;
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      path,
+      width: width ?? 25.0,
+      height: height ?? 25.0,
+      fit: fit,
+      color: color,
+    );
+  }
+}
