a496652b15655b75b7c4e111c42a35ffdb5ccc83
moh <moh94syed@gmail.com>
2023-07-28 14:59:47 +0200

    add

--- lib/src/extensions/icon_base_on_state.dart
+++ lib/src/extensions/icon_base_on_state.dart
@@ -1,20 +1,32 @@
 import 'package:awsome_tools/awsome_tools.dart';
 import 'package:flutter/material.dart';
 
+const iconSize = 40.0;
+
 extension IconBaseOnState on StateAwsome {
   /// ### show icon Base on State Like
-  /// ### [Init] and [Loading] or [Success] State
+  /// ## [Init] and [Loading] or [Success] State
   Widget get iconBaseOnState {
     return switch (this) {
       LoadingState() => const CircularProgressIndicator(
           valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
         ),
-      ErrorState() => const Icon(Icons.error, size: 48, color: Colors.white),
-      WarningState() =>
-        const Icon(Icons.warning, size: 48, color: Colors.white),
-      NetworkErrorState() =>
-        const Icon(Icons.network_check_sharp, size: 48, color: Colors.white),
-      _ => const Icon(Icons.check_circle, size: 48, color: Colors.white)
+      ErrorState() => const Icon(
+          Icons.error,
+          size: iconSize,
+          color: Colors.white,
+        ),
+      WarningState() => const Icon(
+          Icons.warning,
+          size: iconSize,
+          color: Colors.white,
+        ),
+      NetworkErrorState() => const Icon(
+          Icons.network_check_sharp,
+          size: iconSize,
+          color: Colors.white,
+        ),
+      _ => const Icon(Icons.check_circle, size: iconSize, color: Colors.white)
     };
   }
 }
