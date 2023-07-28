0739dda78b9ad24547e2ba8b126b29aabf859012
moh <moh94syed@gmail.com>
2023-07-28 06:37:47 +0200

    add

--- lib/src/extensions/snackbar_ext_awsome.dart
+++ lib/src/extensions/snackbar_ext_awsome.dart
@@ -11,34 +11,36 @@ extension SnackbarAwsome on BuildContext {
   }) {
     //
     late Color backgroundColor = Colors.blue;
+    late IconData iconData;
     switch (state) {
       case LoadingState():
         message = 'Loading...';
         backgroundColor = Colors.blue;
+        iconData = Icons.local_activity;
         break;
       case ErrorState():
         message = state.errorMessage;
         backgroundColor = Colors.red;
+        iconData = Icons.error;
         break;
       case SuccessState():
         message = state.successMessage;
         backgroundColor = Colors.green;
+        iconData = Icons.check;
         break;
     }
     //
     final snackbar = SnackBar(
-      content: setOnTop
-          ? Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  message,
-                  style: regularStyle.copyWith(color: Colors.white),
-                ),
-              ],
-            )
-          : Text(message, style: regularStyle.copyWith(color: Colors.white)),
+      content: Row(
+        children: [
+          Icon(iconData),
+          const SizedBox(width: 5.0),
+          Text(
+            message,
+            style: regularStyle.copyWith(color: Colors.white),
+          ),
+        ],
+      ),
       backgroundColor: backgroundColor,
       duration: duration,
       behavior: setOnTop ? SnackBarBehavior.floating : SnackBarBehavior.fixed,
