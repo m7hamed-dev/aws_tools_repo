046e416f48a9684ccba386b52fffaa82a1997371
moh <moh94syed@gmail.com>
2023-07-28 08:43:37 +0200

    add

--- lib/src/extensions/screen_size_ext.dart
+++ lib/src/extensions/screen_size_ext.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
+enum ScreenTypes { mobile, tablet, web }
+
 extension ScreenSizeAwsome on BuildContext {
   Size get size => MediaQuery.of(this).size;
   bool get isMobileLayout => size.width < 600;
   bool get isTabletLayout => size.width >= 600 && size.width < 1200;
   bool get isWebLayout => size.width >= 1200;
+  ScreenTypes get screenType {
+    if (isMobileLayout) {
+      return ScreenTypes.mobile;
+    }
+    if (isTabletLayout) {
+      return ScreenTypes.tablet;
+    }
+    return ScreenTypes.web;
+  }
 }
