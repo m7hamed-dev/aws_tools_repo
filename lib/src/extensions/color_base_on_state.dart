010af2ab7bb51665b085630d404b6101f91c8001
moh <moh94syed@gmail.com>
2023-07-28 08:29:09 +0200

    add

--- lib/src/extensions/color_base_on_state.dart
+++ lib/src/extensions/color_base_on_state.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 extension ColorBaseOnStarte on StateAwsome {
   Color get color {
     return switch (this) {
-      InitalState() => Colors.white,
-      LoadingState() => Colors.grey,
-      ErrorState() => Colors.red,
+      InitalState() => const Color.fromARGB(255, 239, 232, 232),
+      LoadingState() => const Color.fromARGB(255, 16, 115, 197),
+      ErrorState() => const Color.fromARGB(255, 174, 55, 47),
       _ => configAwsome.appColors.primaryColor,
     };
   }
