96f306cf7b7a9871163ea417163d2562e691d68f
moh <moh94syed@gmail.com>
2023-07-29 08:04:06 +0200

    add some

--- lib/src/network/api_consumer.dart
+++ lib/src/network/api_consumer.dart
@@ -1,35 +1,52 @@
-abstract class APIsConsumer<BodyType, FunReturn> {
-  Future<List<FunReturn>> getALL(
+abstract class APIsConsumer<BodyData, ReturnType> {
+  Future<Result<List<ReturnType>, Exception>> getALL(
     String endpoint, {
     Map<String, String>? headers,
   });
-  Future<FunReturn> getByID(
+  Future<ReturnType?> getByID(
     String endpoint,
     String id, {
     Map<String, String>? headers,
   });
-  Future<FunReturn> post(
+  Future<ReturnType> post(
     String endpoint,
-    BodyType body, {
+    BodyData body, {
     Map<String, String>? headers,
   });
-  Future<FunReturn> put(
+  Future<ReturnType> put(
     String endpoint,
-    BodyType body, {
+    BodyData body, {
     Map<String, String>? headers,
   });
-  Future<FunReturn> delete(
+  Future<ReturnType> delete(
     String endpoint, {
     Map<String, String>? headers,
   });
 }
 
-class ApiError {
-  ApiError(this.statusCode, this.message);
-  final int statusCode;
-  final String message;
-  @override
-  String toString() {
-    return 'ApiError: Status Code $statusCode, Message: $message';
-  }
+// Base Result class
+/// [S] represents the type of the success value
+/// [E] should be [Exception] or a subclass of it
+sealed class Result<S, E extends Exception> {
+  const Result();
 }
+
+final class Success<S, E extends Exception> extends Result<S, E> {
+  const Success(this.value);
+  final S value;
+}
+
+final class Failure<S, E extends Exception> extends Result<S, E> {
+  const Failure(this.exception);
+  final E exception;
+}
+
+// class ApiError {
+//   ApiError(this.statusCode, this.message);
+//   final int statusCode;
+//   final String message;
+//   @override
+//   String toString() {
+//     return 'ApiError: Status Code $statusCode, Message: $message';
+//   }
+// }
