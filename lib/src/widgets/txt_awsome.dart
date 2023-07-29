f838fc5bee93f23c0777d4f6bb41265d83c393d1
moh <moh94syed@gmail.com>
2023-07-29 07:18:00 +0200

    add some

--- lib/src/widgets/txt_awsome.dart
+++ lib/src/widgets/txt_awsome.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 class TxtAwsome extends StatelessWidget {
-  /// Text
-  const TxtAwsome(this.data, {super.key, this.txtColor, this.style});
+  const TxtAwsome(
+    this.data, {
+    super.key,
+    this.style,
+    this.textAlign,
+    this.textDirection,
+    this.locale,
+    this.softWrap,
+    this.overflow,
+    this.textScaleFactor,
+    this.maxLines,
+  });
   final String data;
-  final Color? txtColor;
   final TextStyle? style;
-
-  ///
+  final TextAlign? textAlign;
+  final TextDirection? textDirection;
+  final Locale? locale;
+  final bool? softWrap;
+  final TextOverflow? overflow;
+  final double? textScaleFactor;
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
-    return Text(data, style: style);
+    return Text(
+      data,
+      style: style,
+    );
   }
 }
