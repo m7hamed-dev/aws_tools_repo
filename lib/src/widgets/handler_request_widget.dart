478e734c4e25dc0d41dbfbe30f3c1cf8bc7b594f
moh <moh94syed@gmail.com>
2023-07-29 09:58:14 +0200

    add some

--- lib/src/widgets/handler_request_widget.dart
+++ lib/src/widgets/handler_request_widget.dart
@@ -5,24 +5,21 @@ class HandlerRequesWidgetAwsome<Data> extends StatelessWidget {
   const HandlerRequesWidgetAwsome({
     super.key,
     required this.requestState,
+    required this.success,
     this.init,
     this.loading,
     this.error,
-    required this.success,
-    this.setAnimation = false,
     this.errorMessage = 'Error ',
   });
   final RequestState<Data> requestState;
   final Widget? init, loading, error;
   final Widget success;
-  final bool setAnimation;
   final String errorMessage;
   @override
   Widget build(BuildContext context) {
-    final currentWidget = switch (requestState) {
-      InitalState() => init ?? const CircularProgressIndicator(),
-      LoadingState() => loading ?? const CircularProgressIndicator(),
-      ErrorState() => error ?? TxtAwsome(errorMessage),
+    Widget currentWidget = switch (requestState) {
+      RequestLoading() => loading ?? const CircularProgressIndicator(),
+      RequestError() => error ?? TxtAwsome(errorMessage),
       _ => success
     };
     return currentWidget;
