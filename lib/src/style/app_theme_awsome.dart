d303a4d59482210e565641b6eca3ad21ee0b5065
moh <moh94syed@gmail.com>
2023-09-10 21:41:47 +0200

    review Code and Add Some update some code

--- lib/src/style/app_theme_awsome.dart
+++ lib/src/style/app_theme_awsome.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class AppThemeAwsome {
-  const AppThemeAwsome({this.lightTheme2});
-  // this.darkTheme2,
-  final ThemeData? lightTheme2;
-  // final ThemeData? darkTheme2;
-}
-//   const AppThemeAwsome({
-//     required this.darkTheme,
-//     required this.lightTheme,
-//   });
-//   final DarkThemeAwsome darkTheme;
-//   final LightThemeAwsome lightTheme;
-// }
