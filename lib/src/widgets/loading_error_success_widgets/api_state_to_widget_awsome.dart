cd36ea1053ee7e036dae54a64847d267053a553f
moh <moh94syed@gmail.com>
2023-09-11 02:17:08 +0200

    review and update some code

--- lib/src/widgets/loading_error_success_widgets/api_state_to_widget_awsome.dart
+++ lib/src/widgets/loading_error_success_widgets/api_state_to_widget_awsome.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import '../../../awsome_tools.dart';
 
 class ApiStateToWidget<Data> extends StatelessWidget {
-  /// ### take api State and return widget base on Current State
+  /// ## Take an Api State and Return widget base on Current State
   const ApiStateToWidget({
     super.key,
-    required this.state,
+    required this.apiStates,
     required this.success,
     this.initWidget,
     this.loadingWidget,
@@ -14,23 +14,22 @@ class ApiStateToWidget<Data> extends StatelessWidget {
   });
 
   ///
-  final BaseApiState<Data> state;
+  final BaseApiState<Data> apiStates;
   final Widget? initWidget, loadingWidget, errorWidget;
   final Widget Function(Data data) success;
 
-  ///
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: switch (state) {
-        LoadingState() => loadingWidget ?? const CircularProgressIndicator(),
-        SuccesState() => success(state.data as Data),
-        _ => errorWidget ??
-            TxtAwsome(
-              state.message,
-              style: regularStyle.copyWith(color: state.colorBaseOnStarte),
-            )
-      },
-    );
+    return switch (apiStates) {
+      LoadingState() => loadingWidget ?? const CircularProgressIndicator(),
+      SuccesState() => success(apiStates.data as Data),
+      // onther Cases => error , warning , empty ... ect
+      _ => errorWidget ??
+          TxtAwsome(
+            apiStates.message,
+            style: regularStyle.copyWith(color: apiStates.colorBaseOnStarte),
+            overflow: TextOverflow.clip,
+          )
+    };
   }
 }
