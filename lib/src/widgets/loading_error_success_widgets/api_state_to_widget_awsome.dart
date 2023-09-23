dc3dfd8f41498f3b1b3fcd50927ee44c73006b53
moh <moh94syed@gmail.com>
2023-09-23 02:24:10 +0200

    add

--- lib/src/widgets/loading_error_success_widgets/api_state_to_widget_awsome.dart
+++ lib/src/widgets/loading_error_success_widgets/api_state_to_widget_awsome.dart
@@ -1,35 +1,46 @@
-import 'package:awsome_tools/src/extensions/color_base_on_state.dart';
-import 'package:flutter/material.dart';
 import '../../../awsome_tools.dart';
+import 'package:flutter/material.dart';
+import 'package:awsome_tools/src/extensions/color_base_on_state.dart';
 
 class ApiStateToWidget<Data> extends StatelessWidget {
   /// ## Take an Api State and Return widget base on Current State
   const ApiStateToWidget({
     super.key,
-    required this.apiStates,
+    required this.state,
     required this.success,
     this.initWidget,
     this.loadingWidget,
     this.errorWidget,
+    this.errorWidgetBuilder,
   });
 
   ///
-  final BaseApiState<Data> apiStates;
+  final BaseApiState<Data> state;
   final Widget? initWidget, loadingWidget, errorWidget;
   final Widget Function(Data data) success;
+  final Widget Function(String errorMessage)? errorWidgetBuilder;
 
   @override
   Widget build(BuildContext context) {
-    return switch (apiStates) {
-      InitalState() => initWidget ?? const SizedBox(),
-      LoadingState() => loadingWidget ?? const CircularProgressIndicator(),
-      ErrorState() || WarningState() || NetworkErrorState() => errorWidget ??
-          TxtAwsome(
-            apiStates.message ?? '',
-            style: regularStyle.copyWith(color: apiStates.colorBaseOnStarte),
-            overflow: TextOverflow.clip,
-          ),
-      _ => success(apiStates.data as Data),
-    };
+    return Center(
+      child: switch (state) {
+        InitalState() => initWidget ?? const SizedBox(),
+        LoadingState() => loadingWidget ?? const CircularProgressIndicator(),
+        ErrorState() ||
+        WarningState() ||
+        NetworkErrorState() =>
+          errorWidgetBuilder != null
+              ? errorWidgetBuilder!(state.message ?? 'Error Ocure!')
+              : errorWidget ??
+                  TxtAwsome(
+                    state.message ?? '',
+                    style: regularStyle.copyWith(
+                      color: state.colorBaseOnStarte,
+                    ),
+                    overflow: TextOverflow.clip,
+                  ),
+        _ => success(state.data as Data),
+      },
+    );
   }
 }
