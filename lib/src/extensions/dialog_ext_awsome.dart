eca7b243e0867875135d1e62a10a0cfb90790e7b
moh <moh94syed@gmail.com>
2023-09-24 22:26:03 +0200

    create a document

--- lib/src/extensions/dialog_ext_awsome.dart
+++ lib/src/extensions/dialog_ext_awsome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../network/base_state_awsome.dart';
+import 'package:awsome_tools/awsome_tools.dart';
 import 'package:awsome_tools/src/widgets/dialogs/dialog_awsome.dart';
 
 extension DialogExtension on BuildContext {
@@ -8,7 +8,20 @@ extension DialogExtension on BuildContext {
     BaseState state, {
     bool useAdaptiveDialog = false,
     bool useCutomDesignDialog = true,
+    bool isLoadingDialog = false,
   }) async {
+    /// # show loading
+    if (isLoadingDialog) {
+      await showDialog(
+        context: this,
+        // Prevent dismissing by tapping outside
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return const DialogLoadingBody();
+        },
+      );
+    }
+
     ///# show dialog base on Platform Like (IOS , Android) with Design Behavior
     if (useAdaptiveDialog) {
       await showAdaptiveDialog(
@@ -27,5 +40,33 @@ extension DialogExtension on BuildContext {
         ),
       );
     }
+  }
+}
+
+class DialogLoadingBody extends StatelessWidget {
+  const DialogLoadingBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog.adaptive(
+      elevation: 20.0,
+      contentPadding: const EdgeInsets.all(10.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: configAwsome.defaultBorderRadius,
+      ),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            const SizedBox(width: 10.0),
+            CircularProgressIndicator(
+              strokeWidth: 2.0,
+              color: configAwsome.appColors.primaryColor,
+            ),
+            const SizedBox(width: 20.0),
+            const TxtAwsome('من فضلك انتظر'),
+          ],
+        ),
+      ),
+    );
   }
 }
