2a242927c1d95ef1843cd512562ea212bcba1fde
moh <moh94syed@gmail.com>
2023-09-25 22:09:04 +0200

    add status code extension

--- lib/src/extensions/dialogs/dialog_if_empty.dart
+++ lib/src/extensions/dialogs/dialog_if_empty.dart
@@ -4,9 +4,10 @@ import 'package:awsome_tools/awsome_tools.dart';
 /// # Shows a dialog if the string is empty.
 extension StringExtension on String {
   /// # Shows a dialog if the string is empty.
-  void showDialogIfEmpty(BuildContext context, String dialogMessage) {
+  void showDialogIfEmpty(BuildContext context, String dialogMessage) async {
     if (isEmpty) {
-      context.show(EmptyState(noData: dialogMessage));
+      await context.show(EmptyState(noData: dialogMessage));
+      return;
     }
   }
 }
