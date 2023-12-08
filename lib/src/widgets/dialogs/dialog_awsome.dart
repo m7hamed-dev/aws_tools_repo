084ef5f1b3e7fa798dfb3cb91bb9f9c6004a49d8
moh <moh94syed@gmail.com>
2023-12-08 09:56:21 +0200

    add some update

--- lib/src/widgets/dialogs/dialog_awsome.dart
+++ lib/src/widgets/dialogs/dialog_awsome.dart
@@ -7,7 +7,6 @@ import 'package:awsome_tools/src/extensions/navigator_ext.dart';
 import 'package:awsome_tools/src/extensions/txt_base_on_state.dart';
 import 'package:awsome_tools/src/extensions/icon_base_on_state.dart';
 import 'package:awsome_tools/src/extensions/color_base_on_state.dart';
-import 'package:awsome_tools/src/constants/constant_values_awsome.dart';
 import 'package:awsome_tools/src/widgets/circle_widget_awsom_extension.dart';
 
 class DialogBodyWidget extends StatelessWidget {
