1862f5c0b5a48f199c16521d34e9ff0bd4519161
moh <moh94syed@gmail.com>
2024-07-07 17:56:00 +0300

    remove line from description

--- lib/src/widgets/snackbars/snack_bar_widget_awsome.dart
+++ lib/src/widgets/snackbars/snack_bar_widget_awsome.dart
@@ -2,9 +2,7 @@ import '../txt_awsome.dart';
 import 'package:flutter/material.dart';
 import '../../config/config_awsome.dart';
 import '../../network/base_state_awsome.dart';
-import 'package:awsome_tools/src/extensions/screen_size_ext.dart';
 import 'package:awsome_tools/src/extensions/icon_base_on_state.dart';
-import 'package:awsome_tools/src/extensions/color_base_on_state.dart';
 import 'package:awsome_tools/src/widgets/circle_widget_awsom_extension.dart';
 
 class SnackBarWidgetAwsome {
@@ -26,87 +24,36 @@ class SnackBarWidgetAwsome {
           top: MediaQuery.of(context).size.height * .1,
           left: 0,
           right: 0,
-          child: Material(
-            color: Colors.transparent,
-            child: Container(
-              // padding: const EdgeInsets.all(defaultPaddingAws),
-              width: context.width,
-              decoration: BoxDecoration(
-                color: state.colorBaseOnStarte,
-                boxShadow: [
-                  BoxShadow(
-                    color: state.colorBaseOnStarte.withOpacity(.2),
-                    spreadRadius: 10.0,
-                    blurRadius: 10.0,
-                  )
-                ],
+          child: Row(
+            children: [
+              // icon base on state
+              state.iconBaseOnState.withCircle(
+                color: Colors.white.withOpacity(.12),
+                padding: EdgeInsets.all(configAwsome.defaultPadding),
               ),
-              child: Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Row(
-                    children: [
-                      // icon base on state
-                      state.iconBaseOnState.withCircle(
-                        color: Colors.white.withOpacity(.12),
-                        padding: EdgeInsets.all(configAwsome.defaultPadding),
+              SizedBox(width: configAwsome.defaultPadding),
+              title == null
+                  ? TxtAwsome(message ?? '', color: Colors.white)
+                  : Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TxtAwsome(
+                            title,
+                            color: Colors.white,
+                            overflow: TextOverflow.clip,
+                          ),
+                          // const SizedBox(height: 8.0),
+                          TxtAwsome(
+                            msg,
+                            color: Colors.white,
+                            overflow: TextOverflow.clip,
+                          ),
+                        ],
                       ),
-                      SizedBox(width: configAwsome.defaultPadding),
-                      title == null
-                          ? TxtAwsome(
-                              message ?? '',
-                              // style: boldStyle,
-                              color: Colors.white,
-                            )
-                          : Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TxtAwsome(
-                                    title,
-                                    // style: boldStyle,
-                                    color: Colors.white,
-                                    overflow: TextOverflow.clip,
-                                  ),
-                                  // const SizedBox(height: 8.0),
-                                  TxtAwsome(
-                                    msg,
-                                    // style: mediumStyle,
-                                    color: Colors.white,
-                                    overflow: TextOverflow.clip,
-                                  ),
-                                ],
-                              ),
-                            ),
-                    ],
-                  ),
-                  Positioned(
-                    top: -20,
-                    right: -50,
-                    bottom: 0,
-                    child: const SizedBox(
-                      width: 50.0,
-                      height: 50.0,
-                    ).withCircle(
-                      color: Colors.white.withOpacity(.12),
-                      padding: const EdgeInsets.all(10.0),
                     ),
-                  ),
-                  Positioned(
-                    top: -40,
-                    right: -30,
-                    child: const SizedBox(
-                      width: 50.0,
-                      height: 50.0,
-                    ).withCircle(
-                      color: Colors.white.withOpacity(.12),
-                      padding: const EdgeInsets.all(10.0),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            ],
           ),
         );
       },
